53bd8a82879e8d9660221363871d695b0b78a851
Dreyf <dreyfcesar@gmail.com>
2023-06-29 11:09:18 -0300

    Implementa la impresión de errores y su configuración
    Agrega un gestor de impresión de errores simple (el único de momento)
    y la configuración del gestor de errores del sistema.

--- aplicacion/iniciar.php
+++ aplicacion/iniciar.php
@@ -4,12 +4,13 @@
     use Gof\Gestor\Mensajes\Guardar\GuardarEnArchivo;
     use Gof\Gestor\Registro\Simple\RegistroSimple;
     use Gof\Sistema\MVC\Registros\Errores\GuardarEn;
+    use Gof\Sistema\MVC\Registros\Errores\Impresor as ImpresorDeErrores;
     use Gof\Sistema\MVC\Registros\Errores\Traductor as TraductorDeErrores;
     use Gof\Sistema\MVC\Sistema as SistemaMVC;
 
     // Depuración
-    error_reporting(E_ALL);
-    ini_set('display_errors', true);
+    // error_reporting(E_ALL);
+    // ini_set('display_errors', true);
 
     // Composer
     require '../vendor/autoload.php';
@@ -21,11 +22,16 @@
 
     register_shutdown_function([$errores, 'registrar']);
 
-    // Almacenamiento de errores
+    // Gestión de errores
     $traductorDeErrores = new TraductorDeErrores();
     $errorLog = new Archivo(dirname(__DIR__).'/registros/errores.log');
     $registroLog = new RegistroSimple(new GuardarEnArchivo($errorLog));
     $errores->guardado()->agregar(new GuardarEn($registroLog, $traductorDeErrores));
+    $errores->impresion()->agregar(new ImpresorDeErrores($traductorDeErrores));
+
+    // Configuración del gestor de errores
+    $errores->guardar  = true;
+    $errores->imprimir = true;
 
     // Aplicacion Web
     require 'aplicacion.php';
