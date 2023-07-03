d88232988180bb2fae299ff3ec007c6529d5c806
Dreyf <dreyfcesar@gmail.com>
2023-07-03 18:31:01 -0300

    Agrega un ejemplo de plantilla
    Crea una plantilla de ejemplo que utiliza también datos de ejemplo.

--- aplicacion/Controladores/Index.php
+++ aplicacion/Controladores/Index.php
@@ -9,6 +9,11 @@ class Index extends Controlador
 
     public function indice()
     {
+        $this->datos['web']['nombre'] = 'Margof';
+        $this->datos['titulo'] = 'Un título de ejemplo';
+        $this->datos['descripcion'] = 'Esto es solo un ejemplo de plantilla renderizable por el motor de plantillas Simple.';
+        $this->datos['footer'] = 'Derechos Expirados - 2023';
+        $this->vista()->plantilla('ejemplo');
     }
 
 }
