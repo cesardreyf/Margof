1e759e1c07cd7cfd1bd24f33b9fb6702645bbb3a
Dreyf <dreyfcesar@gmail.com>
2023-08-06 23:22:32 -0300

    Arregla el error en la prueba unitaria del controlador base

--- test/Controladores/Base/ControladorTest.php
+++ test/Controladores/Base/ControladorTest.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace Controladores\Base;
 
 use Controlador\Base\Controlador;
-use Gof\Sistema\MVC\Aplicacion\Criterio\Ipiperf\Interfaz\Controlador as InterfazDelControlador;
+use Gof\Sistema\MVC\Interfaz\Ejecutable;
 use PHPUnit\Framework\TestCase;
 
 class ControladorTest extends TestCase
@@ -19,7 +19,7 @@ class ControladorTest extends TestCase
 
     public function testExtenderDelControladorBase(): void
     {
-        $this->assertInstanceOf(InterfazDelControlador::class, $this->controlador);
+        $this->assertInstanceOf(Ejecutable::class, $this->controlador);
     }
 
 }
